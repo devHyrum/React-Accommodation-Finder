7b2e1550a5b20f931464f70462866420e5218af8
holivera159 <holivera0730@gmail.com>
2024-08-16 23:52:16 -0300

    Design mobile

--- src/components/Main/MainItem.jsx
+++ src/components/Main/MainItem.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import './Main.css'
+import React from "react";
+import "./Main.css";
 
-export default function MainItem({char:{photo, type, beds, rating, title, superHost}}) {
+export default function MainItem({
+  char: { photo, type, beds, rating, title, superHost },
+}) {
   return (
     <div className="stay-item">
-        <div className="smallPicture">
-            <img src={photo} alt="Stay 1"/>
-        </div>
-        <div className="stay-info">
+      <div className="smallPicture">
+        <img src={photo} alt="Stay 1"/>
+      </div>
+      <div className="stay-info">
         {superHost && <span className="super-host">SUPER HOST</span>}
-            <span className="text">{type} {beds !== null && ` . ${beds} beds`}</span>
-            <span className="rating">
-                <img src="/svg/star.svg" alt="star SVG"/>                    
-                {rating}</span>
-        </div>
-        <div className="description">{title}</div>
+        <span className="text">
+          {type} {beds !== null && ` . ${beds} beds`}
+        </span>
+        <span className="rating">
+          <img src="/svg/star.svg" alt="star SVG" />
+          {rating}
+        </span>
+      </div>
+      <div className="description">{title}</div>
     </div>
-  )
+  );
 }
